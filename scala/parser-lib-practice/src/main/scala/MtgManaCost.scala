e424793dbcd550e9414ac9906ea0b7f3a06438d3
p-kino <>
2023-05-15 20:03:27 +0900

    add MtgManaCost parser

---
+++ scala/parser-lib-practice/src/main/scala/MtgManaCost.scala
@@ -0,0 +1,18 @@
+import scala.util.parsing.combinator.JavaTokenParsers
+
+class MtgManaCost extends JavaTokenParsers {
+  def generic: Parser[Any] = floatingPointNumber
+  def colorless: Parser[Any] = rep("◇")
+  def white: Parser[Any] = rep("W")
+  def blue: Parser[Any] = rep("U")
+  def black: Parser[Any] = rep("B")
+  def red: Parser[Any] = rep("R")
+  def green: Parser[Any] = rep("G")
+
+  def manaCost: Parser[Any] =
+    generic~colorless~white~blue~black~red~green
+}
+
+object MtgManaCost extends MtgManaCost {
+  def parseManaCost(arg: String) = parse(manaCost, arg)
+}
