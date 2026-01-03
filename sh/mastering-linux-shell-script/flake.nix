8c957b44bd82507a51d00e1c2f681b1bae5a057c
pkino <pkino@pkino.net>
2026-01-03 11:44:03 +0900

    flakeファイルを用意した

---
+++ sh/mastering-linux-shell-script/flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [ bash ];
+
+        shellHook = ''
+          export PATH="$PWD:$PATH"
+          echo "Current directory added to PATH"
+        '';
+      };
+    };
+}
