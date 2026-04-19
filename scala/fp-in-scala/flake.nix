161541bd05d4c890eee42cd8280ca76803c2f453
l955a6 <75358673+l955a6@users.noreply.github.com>
2026-04-20 00:10:53 +0900

    パッケージ管理をnixに移行

---
+++ scala/fp-in-scala/flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [ pkgs.sbt ];
+        };
+      }
+    );
+}
